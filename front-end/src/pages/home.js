995a2fef02bb65023d394d7b4ac60746ac26d3cc
Abdu-Ak <abduak1425378@gmail.com>
2023-02-20 20:05:16 +0530

    first -navbar

---
+++ front-end/src/pages/home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../component/Navbar/Navbar'
+import Home from '../component/Home/Home'
+function home() {
+  return (
+    <div>
+        <Navbar />
+        <Home />
+    </div>
+  )
+}
+
+export default home
---
+++ front-end/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: { 
+       colors: {
+      secondory: "#F8F4EB",   
+    },
+  },
+  },
+  plugins: [],
+
+
+}
